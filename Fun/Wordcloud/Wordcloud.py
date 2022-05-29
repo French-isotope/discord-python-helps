29026da80a97823e0d70e4f8cffe1091f15f34b7
French-isotope <french-isotope@gmail.com>
2022-05-29 02:31:43 +0200

    add worldcloud and matrix shuffle with numpy

---
+++ Fun/Wordcloud/Wordcloud.py
@@ -0,0 +1,11 @@
+from wordcloud import WordCloud, STOPWORDS
+import numpy as np
+import matplotlib.pyplot as plt
+import PIL.Image
+
+text = open('github.txt', 'r').read()
+
+wc = WordCloud(stopwords=STOPWORDS).generate(text)
+plt.imshow(wc)
+plt.axis("off")
+plt.show()
