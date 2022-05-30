2875518c503a1459214eadd87a4643abdee45d6d
French-isotope <French.isotope@gmail.com>
2022-05-30 15:48:59 +0200

    add calculs for avering the value of inflation evolution

--- test_bf_integration3.py
+++ test_bf_integration3.py
@@ -4,15 +4,27 @@ from pandas_datareader import wb
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 
-data = wb.download(indicator='FP.CPI.TOTL.ZG',country='all',start=2016,end=2020).reset_index()
+data = wb.download(indicator='FP.CPI.TOTL.ZG',country='all',start=2018,end=2020).reset_index()
 
 data = data[147:]
 
 data.head()
 
-inflation = data.pivot_table(index='country', columns='year', values ='FP.CPI.TOTL.ZG' ).reset_index()
+inflation = data.pivot_table(index='country', columns='year', values='FP.CPI.TOTL.ZG').reset_index()
 inflation.head()
 
-inflation['Evol_Inflation'] = round(((inflation['2020']-inflation['2018'])/inflation['2018'])*100,2)
+inflation['Evol_Inflation'] = round(((inflation['2020']-inflation['2018'])/inflation['2018'])*100, 2)
 
-print(inflation.head())
+#print(inflation.head(300))
+print(inflation['Evol_Inflation'])
+
+val_is_null = pd.isnull(inflation['Evol_Inflation'])
+
+good_values = inflation['Evol_Inflation'][val_is_null == False]
+
+#print(good_values)
+
+average_inflation = sum(good_values) / len(good_values)
+
+#print(average_inflation)
+
