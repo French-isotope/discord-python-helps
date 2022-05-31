8a0b99835ae5bc2c2cad0765010275fea2b803d6
French-isotope <French.isotope@gmail.com>
2022-05-31 11:14:21 +0200

    add oop stuff directory and how to create own special methods in classes

---
+++ test_before_integration6.py
@@ -0,0 +1,46 @@
+class Fraction:
+    """ that represents a fraction consisting of a numerator and a denominator.
+    """
+
+    def __init__(self, numerator, denominator):
+        """initialize numerator and denominator"""
+        self.numerator = numerator
+        self.denominator = denominator
+
+    def __str__(self):
+        """returns a string representation of the fraction"""
+        return f"{self.numerator}/{self.denominator}"
+
+    def __repr__(self):
+        """returns a string representation of the fraction"""
+        return f"{self.numerator}/{self.denominator}"
+
+    def __add__(self, other):
+        """returns the sum of two fractions"""
+        numerator = self.numerator * other.denominator + self.denominator * other.numerator
+        denominator = self.denominator * other.denominator
+        return Fraction(numerator, denominator)
+
+    def __sub__(self, other):
+        """subtract"""
+        numerator = self.numerator * other.denominator - self.denominator * other.numerator
+        denominator = self.denominator * other.denominator
+        return Fraction(numerator, denominator)
+
+    def __mul__(self, other):
+        """multiply"""
+        numerator = self.numerator * other.denominator * self.denominator * other.numerator
+        denominator = self.denominator * other.denominator
+        return Fraction(numerator, denominator)
+
+    def __truediv__(self, other):
+        """divide"""
+        numerator = self.numerator * other.denominator
+        denominator = self.denominator * other.numerator
+        return Fraction(numerator, denominator)
+
+if __name__ == "__main__":
+    obj1 = Fraction(2, 10)
+    print(obj1.__str__())
+
+    
