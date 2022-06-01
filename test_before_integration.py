d714f61541e3113984ff829094ae91b9435c901b
French-isotope <French.isotope@gmail.com>
2022-06-01 18:36:10 +0200

    add new jupyer notebook and options for pd.dataframes

--- test_before_integration.py
+++ test_before_integration.py
@@ -37,18 +37,73 @@ def clean_text(rgx_list, text):
         new_text = re.sub(rgx_match, '', new_text)
     return new_text
 
-print(gdp['Zone'])
+#print(gdp['Zone'])
 
-gdp['Zone'].str.replace(" \[\d{1,3}\]","")
+#gdp['Zone'].str.replace(" \[\d{1,3}\]","")
 
-print(gdp['Zone'].str.replace(" \[\d{1,3}\]","", regex=True))
+#print(gdp['Zone'].str.replace(" \[\d{1,3}\]","", regex=True))
 
-gdp['Zone'] = gdp['Zone'].str.replace(" \[\d{1,3}\]","", regex=True)
+#gdp['Zone'] = gdp['Zone'].str.replace(" \[\d{1,3}\]","", regex=True)
 
 #print(clean_text([' \[\d{1,3}\]'], str(gdp['Zone'])))
 #replaced = re.sub(' \[\d{1,3}\]', '', str(gdp['Zone']))
 #print(replaced)
 
-print(gdp)
+#print(gdp)
 
-print(gdp.sort_values('Evol_gdp', ascending=False))
+#print(gdp.sort_values('Evol_gdp', ascending=False))
+
+
+
+import pycountry
+import gettext
+import copy
+eng_to_french = gettext.translation('iso3166', pycountry.LOCALES_DIR,languages=['fr'])
+eng_to_french.install()
+french_to_eng = copy.copy(eng_to_french) #Make a copy
+french_to_eng._catalog = {} #Remove the catalog
+
+for key in eng_to_french._catalog.keys():
+    french_to_eng._catalog[eng_to_french._catalog[key]] = key #replace the key with the entry and vice versa
+
+#Verify
+_ = french_to_eng.gettext
+#translated_string = _("Iles Salomon")
+#print(translated_string) #This should print "Germany"
+
+
+
+
+#######################################
+
+
+import pandas as pd
+from pandas_datareader import wb
+
+pd.set_option('display.max_rows', 5000)
+pd.set_option('display.max_columns', 5000)
+pd.set_option('display.width', 10000)
+pd.set_option('display.expand_frame_repr', False)
+
+
+aliment_17 = pd.read_csv('DisponibiliteAlimentaire_2017.csv')
+population = pd.read_csv('Population_2000_2018.csv')
+gdp = pd.read_csv('gdp_2018_2020.csv')
+inflation = wb.download(indicator='FP.CPI.TOTL.ZG',country='all',start=2018,end=2022).reset_index()
+
+#print(aliment_17.head(3))
+
+aliment_pivot = aliment_17.pivot(index=['Zone', 'Produit', 'Année'], columns='Élément', values='Valeur').reset_index()
+
+#print(aliment_pivot.head(3))
+
+liste_viande = ['Viande de Volailles']
+
+aliment_viande = aliment_pivot[aliment_pivot['Produit'].isin(liste_viande)].reset_index()
+#print(aliment_viande.head())
+
+
+inflation = inflation[196:]
+
+print(inflation.head())
+
