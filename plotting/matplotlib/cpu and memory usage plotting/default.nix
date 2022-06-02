dbc593fcda818d9f13ffb09c84f6b2833ef0baac
French-isotope <French.isotope@gmail.com>
2022-06-02 13:56:43 +0200

    refactor default.nix for the packages

--- plotting/matplotlib/cpu and memory usage plotting/default.nix
+++ plotting/matplotlib/cpu and memory usage plotting/default.nix
@@ -1,5 +1,5 @@
 let pkgs = import ./nixpkgs.nix {};
-  pythonEnv = pkgs.python3.withPackages(p : [p.numpy p.matplotlib p.dateutils]);
+  pythonEnv = pkgs.python3.withPackages(p : [p.matplotlib p.dateutils p.psutil p.pyttsx3]);
 in pkgs.mkShell {
   buildInputs = [ pythonEnv ];
 }
