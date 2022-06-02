32468386e43b50f912c9da4e019a6efa3bf22c85
French-isotope <French.isotope@gmail.com>
2022-06-02 20:50:20 +0200

    adapt nix conf for plot particules that moves

--- plotting/matplotlib/plot particules that moves/default.nix
+++ plotting/matplotlib/plot particules that moves/default.nix
@@ -1,5 +1,5 @@
 let pkgs = import ./nixpkgs.nix {};
-  pythonEnv = pkgs.python3.withPackages(p : [p.numpy p.matplotlib p.dateutils]);
+  pythonEnv = pkgs.python3.withPackages(p : [p.matplotlib]);
 in pkgs.mkShell {
   buildInputs = [ pythonEnv ];
 }
