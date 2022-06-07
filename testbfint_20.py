48dc20f6a177f79106ff67fd318d77a8c5002720
French-isotope <French.isotope@gmail.com>
2022-06-07 18:36:20 +0200

    refactor clustering countries

--- testbfint_20.py
+++ testbfint_20.py
@@ -1,21 +1,12 @@
 import json
 
 clusters = "clusters.csv"
-
-
 countries_by_clusters = dict()
 
 with open(clusters, 'r', encoding='utf-8') as f:
     k_pays = f
-
     for line in k_pays:
-
-        try:
-            num, k, country = line.split(',', maxsplit=2)
-        except Exception as e:
-            print(f'Exception when unpacking csv datas : {e}')
-
-        country = country.replace('"', '')
+        num, k, country = line.replace('"', '').split(',', maxsplit=2)
         if not k in countries_by_clusters and not k.isalpha():
             countries_by_clusters[k] = [country.strip('\n')]
         elif k in countries_by_clusters and not k.isalpha():
