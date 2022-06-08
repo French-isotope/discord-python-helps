a644cf52c3e4ed3a038eed38b006d1caa5dbc2ef
French-isotope <French.isotope@gmail.com>
2022-06-08 18:49:11 +0200

    add recreate recursively directory tree

--- Torrent/ktv_torrents/main.py
+++ Torrent/ktv_torrents/main.py
@@ -1,7 +1,10 @@
 import os
 import json
+from pathlib import Path
 
-path_to_files = "C:/torrents_project"
+
+
+path_to_files = "C:\\torrents_project"
 
 rights_files = dict()
 
@@ -30,13 +33,70 @@ for root, dirs, files in os.walk(path_to_files):
 
 test_dict = dict()
 
+
+
 def listdirs(rootdir):
     for it in os.scandir(rootdir):
         if it.is_dir():
-            
-            it.path
+            test = it.path.split('\\')[1:]
+            if not test[0] in test_dict:
+                test_dict[test[0]] = []
+                print(f'test : {test}')
             listdirs(it)
 
 listdirs(path_to_files)
 
+print(json.dumps(test_dict, indent=4))
 
+
+"""
+def listdirs(rootdir):
+    for path in Path(rootdir).iterdir():
+        if path.is_dir():
+            print(path)
+            listdirs(path)
+
+
+listdirs(path_to_files)
+"""
+
+test_dict2 = dict()
+
+def create_subdirs_in_dict(split_dir):
+    for dir in split_dir:
+        test_dict2
+
+
+arr = []
+for root, dirs, files in os.walk(path_to_files):
+    for dir in dirs:
+        if not dir in rights_files:
+            rights_files[dir] = []
+
+    for filename in files:
+        sub_dir = root.split("/")[1:]
+        arr.append(os.path.join(root, filename))
+
+#print(rights_files)
+
+arr2 = []
+
+for item in arr:
+#    arr2.append(item.replace('/', '\\'))
+    arr2.append(item.replace('C:\\torrents_project\\', ''))
+
+
+print(f'arr2 : {arr2}')
+
+
+d = dict()
+for path in arr2:
+    parent = d
+    for dir in path.split('\\'):
+        if dir not in parent:
+            print(f'parent : {parent}')
+            parent[dir] = dict()
+        parent = parent[dir]
+
+
+print(json.dumps(d, indent=4))
