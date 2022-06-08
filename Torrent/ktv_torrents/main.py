4ea59069c0ec4ba85e710d562d18da585d4db2e6
French-isotope <French.isotope@gmail.com>
2022-06-08 20:34:17 +0200

    add function to recreate arborescence that works

--- Torrent/ktv_torrents/main.py
+++ Torrent/ktv_torrents/main.py
@@ -117,19 +117,82 @@ print(print_dict(d))
 
 print('loliliolzefrlsdjhfgkljsdhfg\n\n\n')
 
+result = []
+path = []
+from copy import copy
+
+
+# i is the index of the list that dict_obj is part of
+def find_path(dict_obj, i=None):
+    for k, v in dict_obj.items():
+        # add key to path
+        path.append(k)
+        if isinstance(v, dict):
+            # continue searching
+            find_path(v, i)
+        if isinstance(v, list):
+            # search through list of dictionaries
+            for i, item in enumerate(v):
+                # add the index of list that item dict is part of, to path
+                path.append(i)
+                if isinstance(item, dict):
+                    # continue searching in item dict
+                    find_path(item, i)
+                # if reached here, the last added index was incorrect, so removed
+                path.pop()
+        if len(v) == 0:
+            # add path to our result
+            result.append(copy(path))
+        # remove the key added in the first line
+        if path != []:
+            path.pop()
+
+
+# default starting index is set to None
+find_path(d)
+
+for resultat in result:
+    print(resultat)
+
+print('loliliolzefrlsdjhfgkljsdhfg\n\n\n')
+
+
+
+
+
+
+
+
+
+
+
 def iterdict(dir, path):
-    for k, v in dir.items():
-        path = f'{path}/{k}'
-        if type(v) == dict:
-            print(path)
-            iterdict(v, path)
+    for key, value in dir.items():
+        # file (at the bottom of the tree)
+        if len(value) == 0:
+#            print(f'key: {key} value: {value}')
+            path2 = f'{path}/{key}'
+            print(f'v == 0 : {path2}')
+
+        # directory (somewhere in the tree)
         else:
-            print(v)
+            if os.path.exists(os.path.join(f'{path}/{key}')):
+                path = f'{path}/{key}'
+                print(f'path exist : {path}')
+                iterdict(value, path)
+            else:
+                path = f'{path}/{key}'
+#                print(f'pas touche {path}')
+                iterdict(value, path)
+
 
 
 print(type(d))
 print(iterdict(d, path_to_files))
 
+
+
+# mkv avi mp4
 
 
 
