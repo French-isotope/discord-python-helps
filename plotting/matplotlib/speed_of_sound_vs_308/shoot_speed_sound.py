53afefcd89f134d5e2518d966427298ed37f4e66
French-isotope <French.isotope@gmail.com>
2022-06-10 14:53:11 +0200

    add values for speed calculation of .308 caliber

--- plotting/matplotlib/speed_of_sound_vs_308/shoot_speed_sound.py
+++ plotting/matplotlib/speed_of_sound_vs_308/shoot_speed_sound.py
@@ -27,3 +27,21 @@ datas_308["Range (meters)"] = datas_308["Range (yards)"] * yards_to_meters_facto
 print(datas_308["Vélocité (ms)"])
 
 print(datas_308)
+
+# https://sciencing.com/what-does-g-force-mean-13710432.html
+
+#
+# (C) is the drag coefficient of the bullet,
+# (ρ) is the air density,
+# (A) is the area of the bullet,
+# (t) is the time of flight and
+# (m) is the mass of the bullet.
+#
+
+drag_coef   = 0.295 #
+air_density = 1.2 # kg/cubic meter at normal pressure and temperature
+bullet_area = 4.8 * 10**-5 # m² cross-sectional area
+flight_time = 0
+bullet_mass = 0
+
+print(bullet_area)
