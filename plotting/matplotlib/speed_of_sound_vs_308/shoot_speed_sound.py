c5309cb185688dd28934c5c1743fbc591b9ec556
French-isotope <French.isotope@gmail.com>
2022-06-10 14:24:48 +0200

    add column convert yards to meters and parameters for pd options

--- plotting/matplotlib/speed_of_sound_vs_308/shoot_speed_sound.py
+++ plotting/matplotlib/speed_of_sound_vs_308/shoot_speed_sound.py
@@ -2,12 +2,15 @@ import pandas as pd
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 10000)
+pd.set_option('display.expand_frame_repr', False)
 
 # Value in m/s = value in fps × 0.3048
 
 sound_speed_ms = 343
 
 feet_to_meters_factor = 0.3048
+yards_to_meters_factor = 0.9144
 
 win_308_speed_fps = 2650
 
@@ -15,11 +18,11 @@ win_308_speed_ms = win_308_speed_fps * feet_to_meters_factor
 
 datas_308 = pd.read_csv("308_chart.csv")
 
-velocity_in_ms = datas_308["Velocity (fps)"] * feet_to_meters_factor
-
 # Add new column velocity (ms)
-datas_308["Vélocité (ms)"] = velocity_in_ms
+datas_308["Vélocité (ms)"] = datas_308["Velocity (fps)"] * feet_to_meters_factor
 
+# Add new column range in meters
+datas_308["Range (meters)"] = datas_308["Range (yards)"] * yards_to_meters_factor
 
 print(datas_308["Vélocité (ms)"])
 
